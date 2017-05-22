178b1cb4a7402ae1937a3d710163c87f3ed4fd4c
Charlie Hanacek <charlie@czhanacek.com>
2017-05-21 20:23:32 -0700

    Fixed landing page controller test

--- test/controllers/landing_page_controller_test.rb
+++ test/controllers/landing_page_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class LandingPageControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get landing_page_index_url
+    get landing_page_url
     assert_response :success
   end
 
