bcd1914df3512dcc694a7076817f4b437f52b9de
Nick Vasicek <nicholas.vasicek@wsu.edu>
2017-06-28 16:07:14 -0700

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,23 +15,21 @@ Officer.delete_all
 Question.delete_all
 Rso.delete_all
 
-rso.create!(name: 'Association for Computer Machinery',
-            shortName: 'ACM',
-            category: 'Computer Science')
-
-rso.create!(name: 'Knitting Club',
-            shortName: 'KNIT',
-            category: 'Special Interest')
-
-rso.create!(name: 'Mountain Biking Club',
-            shortName: 'MBC',
-            category: 'Workout')
-
 question.create!(title: 'Would you rather watch a movie or go bowling?'
-                 backgroundImage: 'sampleimage.jpeg')
+                 backgroundImage: 'sampleimage.jpg')
 
-answer.create!(title: 'Dim the lights.'
+question.create!(title: 'Do you prefer painting or gaming?'
+                 backgroundImage: 'sampleimage.jpg')
+
+
+answer.create!(title: 'Dim the lights and start popping the corn.'
                icon: 'movie_picture.jpeg')
+
+answer.create!(title: 'Painting!'
+               icon: 'paintbrush.jpeg')
+
+answer.create!(title: 'Gaming!'
+               icon: 'video_game_picture.jpeg')
 
 Answer.create!(title: 'Ill get my shoes!')
 
@@ -41,14 +39,6 @@ Keyword.create!(word: 'outdoorsy')
 
 Keyword.create!(word: 'nerdyAF')
 
-Officer.create!(firstName: 'Tyler'
-	            lastName: 'Walker'
-	            email: 'tyler.p.walker@wsu.edu'
-	            phone: '2061234567')
-=======
-Officer.destroy_all
-Rso.destroy_all
-
 Officer.create([
   {id: 1, first: 'Nick', last: 'Vasicek', phone: '555-555-5555', email: 'nick@email.com'},
   {id: 2, first: 'Charlie', last: 'Hanacek', phone: '555-555-5555', email: 'charlie@email.com'},
@@ -56,7 +46,10 @@ Officer.create([
   ])
 
 Rso.create([
-  {id: 1, name: 'Association of Computer Machinery', nickname: 'ACM', officer_ids: [1, 2]},
-  {id: 2, name: 'Linux Users Group', nickname: 'LUG', officer_ids: [1, 2, 3]}
+  {id: 1, name: 'Association of Computer Machinery', nickname: 'ACM', officer_ids: [1, 2], category: 'Special Interest'},
+  {id: 2, name: 'Linux Users Group', nickname: 'LUG', officer_ids: [1, 2, 3], category: 'Special Interest'},
+  {id: 3, name: 'Knitting Club', nickname: 'Knit', officer_ids: [2], category: 'Special Interest'},
+  {id: 4, name: 'Mountain Biking Club', nickname: 'MBC', officer_ids: [1, 2], category: 'Workout'}
+
   ])
 >>>>>>> 319c9c202264785e094863eedd7f6ee73d55d8a6
