d8b17817e6a3bdf400aef24760ec41b69a4a2326
Nick Vasicek <nicholas.vasicek@wsu.edu>
2017-06-28 18:11:20 -0700

    Improved seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,30 +15,32 @@ Officer.delete_all
 Question.delete_all
 Rso.delete_all
 
-question.create!(title: 'Would you rather watch a movie or go bowling?'
-                 backgroundImage: 'sampleimage.jpg')
+Question.create([
+  {id: 1, title: 'Would you rather play a board game or go biking?', backgroundImage: 'sampleimage.jpg')},
+  {id: 2, title: 'Do you prefer painting or gaming?', backgroundImage: 'sampleimage2.jpg')},
+  {id: 3, title: 'Would you rather knit something or write an algorithm?', backgroundImage: 'sampleimage3.jpg')},
+  {id: 4, title: 'Do you prefer to go out or stay in?', backgroundImage: 'sampleimage4.jpg')}
+  {id: 4, title: 'Would you rather go to the a nude bike ride to raise awareness or write a novel?', backgroundImage: 'sampleimage5.jpg')}
+])
 
-question.create!(title: 'Do you prefer painting or gaming?'
-                 backgroundImage: 'sampleimage.jpg')
+Answer.create([
+  {id: 1, title: 'Yahtzee!', icon: 'board_game.jpg'},
+  {id: 2, title: 'Painting!', icon: 'paintbrush.jpg'},
+  {id: 3, title: 'Gaming!', icon: 'video_game.jpg'},
+  {id: 4, title: 'Write code!', icon:'binary_code.jpg'},
+  {id: 5, title: 'Biking!', icon: 'bicycle.jpg'},
+  {id: 6, title: 'Knitting!', icon: 'knitting_needles.jpg'}
+  {id: 7, title: 'Naked Biking for Awareness!', icon: 'blurred_bikers.jpg'}
+  {id: 8, title: 'Write a novel!', icon: 'coffee_and_typewriter.jpg'}
+])
 
-
-answer.create!(title: 'Dim the lights and start popping the corn.'
-               icon: 'movie_picture.jpeg')
-
-answer.create!(title: 'Painting!'
-               icon: 'paintbrush.jpeg')
-
-answer.create!(title: 'Gaming!'
-               icon: 'video_game_picture.jpeg')
-
-Answer.create!(title: 'Ill get my shoes!')
-
-Keyword.create!(word: 'crafty')
-
-Keyword.create!(word: 'outdoorsy')
-
-
-Keyword.create!(word: 'nerdyAF')
+Keyword.create([
+  {id: 1, word: 'crafty', weight: 6},
+  {id: 2, word: 'outdoorsy', weight: 8},
+  {id: 3, word: 'nerdyAF', weight: 8},
+  {id: 4, word: 'progressive/activist', weight: 10},
+  {id: 5, word: 'fit', weight: 5}
+  ])
 
 Officer.create([
   {id: 1, first: 'Nick', last: 'Vasicek', phone: '555-555-5555', email: 'nick@email.com'},
@@ -47,10 +49,9 @@ Officer.create([
   ])
 
 Rso.create([
-  {id: 1, name: 'Association of Computer Machinery', nickname: 'ACM', officer_ids: [1, 2], category: 'Special Interest'},
-  {id: 2, name: 'Linux Users Group', nickname: 'LUG', officer_ids: [1, 2, 3], category: 'Special Interest'},
-  {id: 3, name: 'Knitting Club', nickname: 'Knit', officer_ids: [2], category: 'Special Interest'},
-  {id: 4, name: 'Mountain Biking Club', nickname: 'MBC', officer_ids: [1, 2], category: 'Workout'}
-
+  {id: 1, name: 'Association of Computer Machinery', nickname: 'ACM', officer_ids: [1, 2], keyword_ids: [3], keyword_weights: [7]},
+  {id: 2, name: 'Linux Users Group', nickname: 'LUG', officer_ids: [1, 2, 3], keyword_ids: [3], keyword_weights: [9]},
+  {id: 3, name: 'Knitting Club', nickname: 'KNIT', officer_ids: [2], keyword_ids: [1, 2], keyword_weights: [7, 1]},
+  {id: 4, name: 'Mountain Biking Club', nickname: 'MBC', officer_ids: [1, 2], keyword_ids: [2], keyword_weights: [6]}
   ])
 >>>>>>> 319c9c202264785e094863eedd7f6ee73d55d8a6
