5c7612265f622e5ec0f5634627d141ec6f5fc2d9
Charlie Hanacek <charlie@czhanacek.com>
2017-06-29 10:01:22 -0700

    created partial for questions

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,4 +1,7 @@
 class SurveyController < ApplicationController
   def index
+    @questions = Question.all
+    #@questions = [{:title => "How large is your hometown?", :answers => ["big", "small", "medium"]}, {:title => "How large is your hometown?", :answers => ["big", "small", "medium"]}]
+    puts @questions.size
   end
 end
