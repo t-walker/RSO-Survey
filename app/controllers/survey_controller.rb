69709f133d7ff0832edf4120d5f3cde8fe339550
Charlie Hanacek <charlie@czhanacek.com>
2017-07-05 17:08:26 -0700

    wrote controller and partial code to display all the questions on the survey page

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -2,7 +2,7 @@ class SurveyController < ApplicationController
   def index
     @questionList = Question.all
     #@questions = [{:title => "How large is your hometown?", :answers => ["big", "small", "medium"]}, {:title => "How large is your hometown?", :answers => ["big", "small", "medium"]}]
-
-    puts @questionList
+    
+    puts @questionList[0].question
   end
 end
