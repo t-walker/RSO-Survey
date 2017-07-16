3ef70dfc18eee158a8479aceecb353401a20c3c7
Charlie Hanacek <charlie@czhanacek.com>
2017-07-16 12:56:09 -0700

    Created unit tests for creating questions without titles and answers that didn't belong to questions

--- test/controllers/survey_controller_test.rb
+++ test/controllers/survey_controller_test.rb
@@ -17,6 +17,13 @@ class ManageSurveyControllerTest < ActionDispatch::IntegrationTest
     assert_not question.save, "Saved question without answers"
   end
 
+  test "should not create question with no title" do
+    question = Question.create()
+    question.save(:validate => false)
+    question.answers.create([{answer_title: "41"}, {answer_title: "42"}])
+    assert_not question.save, "Saved question without question title"
+  end
+
   test "should not create question with one answer" do
     question = Question.create(question_title: "What is the meaning of life?")
     question.save(:validate => false)
@@ -29,5 +36,10 @@ class ManageSurveyControllerTest < ActionDispatch::IntegrationTest
     question.save(:validate => false)
     question.answers.create([{answer_title: "41"}, {answer_title: "42"}, {answer_title: "43"}, {answer_title: "44"}, {answer_title: "45"}])
     assert_not question.save, "Saved question with five answers"
+  end
+
+  test "should not create answer that is not linked to a question" do
+    answer = Answer.create()
+    assert_not answer.save, "Saved orphan answer"
   end
 end
