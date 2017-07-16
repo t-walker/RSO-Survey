06ed08932f2148b6a31f63e8add6d7c5e4933463
Charlie Hanacek <charlie@czhanacek.com>
2017-07-16 12:49:50 -0700

    Wrote tests to check if there were more than 4 or less than 2 answers for a question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,16 @@
 class Question < ApplicationRecord
-  validates :answers, presence: true
+  def answer_limits
+    answersNumber = Answer.where(:question_id => self.id).count
+    if !(answersNumber >= 2)
+      errors.add(:answers, "must have 2 or more answers")
+    elsif !(answersNumber <= 4)
+      errors.add(:answers, "must have 4 or less answers")
+    end
+  end
+
+  validates :answers, :presence => true
+  validate :answer_limits
   has_many :answers
+
+
 end
--- test/controllers/survey_controller_test.rb
+++ test/controllers/survey_controller_test.rb
@@ -16,4 +16,18 @@ class ManageSurveyControllerTest < ActionDispatch::IntegrationTest
     question.question_title = "What is the meaning of life?"
     assert_not question.save, "Saved question without answers"
   end
+
+  test "should not create question with one answer" do
+    question = Question.create(question_title: "What is the meaning of life?")
+    question.save(:validate => false)
+    question.answers.create(answer_title: "42")
+    assert_not question.save, "Saved question with one answer"
+  end
+
+  test "should not create question with five answers" do
+    question = Question.create(question_title: "What is the meaning of life?")
+    question.save(:validate => false)
+    question.answers.create([{answer_title: "41"}, {answer_title: "42"}, {answer_title: "43"}, {answer_title: "44"}, {answer_title: "45"}])
+    assert_not question.save, "Saved question with five answers"
+  end
 end
