796538a96984d32703a647640b6d11b7342ae2ba
Charlie Hanacek <charlie@czhanacek.com>
2017-07-23 18:53:55 -0700

    Wrote tests enforcing length of RSO names and nicknames

--- test/controllers/rso_controller_test.rb
+++ test/controllers/rso_controller_test.rb
@@ -20,4 +20,18 @@ class RsoControllerTest < ActionDispatch::IntegrationTest
     rso = Rso.create(name: "Linux Users Group")
     assert_not rso.save, "Saved an RSO without a nickname"
   end
+
+  test "should not create an RSO with a name longer than 255 characters" do
+    # 300+ character-long name
+    rso = Rso.create(name: "Linux Users GroupLinux Users GroupLinux Users GroupLinux
+     Users GroupLinux Users GroupLinux Users GroupLinux Users GroupLinux Users
+     GroupLinux Users GroupLinux Users GroupLinux Users GroupLinux Users GroupLinux
+     Users GroupLinux Users GroupLinux Users GroupLinux Users GroupLinux Users GroupLinux Users Group")
+    assert_not rso.save, "Saved an RSO with a name longer than 255 characters"
+  end
+  test "should not create an RSO with a nickname longer than 50 characters" do
+    # 50+ character-long nickname
+    rso = Rso.create(name: "LUGLUGLUGLUGLUGLUGLUGLUGLUGLUGLUGLUGLUGLUGLUGLUGLUGLUG")
+    assert_not rso.save, "Saved an RSO with a name longer than 255 characters"
+  end
 end
