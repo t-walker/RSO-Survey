d5fcebecbba0ceaa6ee96d2c321965fc743a69cc
Charlie Hanacek <charlie@czhanacek.com>
2017-08-11 23:26:19 -0700

    Updated the survey management page to reflect latest design practices and add question edit and delete functionality

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -3,18 +3,73 @@ class SurveyController < ApplicationController
     @questions = Question.order(:position)
   end
 
+  def edit_question
+    @q = Question.find(params[:id])
+  end
+
+  def modify_question
+    q = Question.find(params[:id])
+    if(params[:position].to_i != q.position)
+      oldPosition = q.position
+      newPosition = params[:position].to_i
+      lastPosition = Question.all.order(position: :desc).first.position
+      # If the position submitted by the user is greater than the last position in the survey,
+      # just make the question the last one in the survey.
+      questionsBetween = []
+      if(newPosition > lastPosition)
+        newPosition = lastPosition
+        questionsBetween = Question.where("position >= ?", oldPosition).order(position: :desc)
+        questionsBetween.each do |q|
+          q.position -= 1
+        end
+      else
+        if(newPosition < oldPosition)
+          questionsBetween = Question.where("position >= ? AND position < ?", newPosition, oldPosition).order(position: :desc)
+          questionsBetween.each do |question|
+            question.position += 1
+          end
+        else
+          questionsBetween = Question.where("position > ? AND position <= ?", oldPosition, newPosition).order(position: :desc)
+          questionsBetween.each do |question|
+            question.position -= 1
+          end
+        end
+      end
+      questionsBetween.each do |question|
+        question.save!
+      end
+    end
+    q.update(:question_title => params[:question_title], :position => newPosition)
+    if(q.valid?)
+      flash[:success] = "Question updated successfully"
+    else
+      flash[:error] = "Question not updated: " + q.errors.full_messages.join(", ")
+    end
+    redirect_to controller: 'survey', action: 'edit_question', id: params[:id]
+  end
+
+
   def create_question
     @question = Question.create(question_title: params[:question_title])
     if(params[:position] == nil)
-      @question.position = @question.id # Did this because it is currently the convention in our seed data.
+      # Add the question to the end of the survey
+      lastPosition = Question.all.order(position: :desc).first.position
+      @question.position = lastPosition + 1
     else
-      @question.position = params[:position]
-      @questionsAfter = Question.where("position >= ?", params[:position].to_i).order(position: :desc)
-      @questionsAfter.each do |q|
-        q.position += 1
-      end
-      @questionsAfter.each do |q|
-        q.save!
+      lastPosition = Question.all.order(position: :desc).first.position
+      puts lastPosition
+      # If the position submitted by the user is greater than the last position in the survey,
+      # just make the question the last one in the survey.
+      if(params[:position].to_i > lastPosition)
+        @question.position = lastPosition + 1
+      else
+        @questionsAfter = Question.where("position >= ?", params[:position].to_i).order(position: :desc)
+        @questionsAfter.each do |q|
+          q.position += 1
+        end
+        @questionsAfter.each do |q|
+          q.save!
+        end
       end
     end
     @question.save(:validate => false)
@@ -38,8 +93,14 @@ class SurveyController < ApplicationController
       nextOrder += 1
       @question.answers.create(answer_title: params[:answer4], position: nextOrder)
     end
-    @question.save!
-    flash[:success] = "Question created successfully!"
+
+    if(@question.valid?)
+      flash[:success] = "Question created successfully!"
+    else
+      flash[:error] = "Question not created: " + @question.errors.full_messages.join(", ")
+      @question.destroy
+    end
+
     redirect_to action: "manage"
   end
 
@@ -52,14 +113,32 @@ class SurveyController < ApplicationController
     answer = Answer.find(params[:answer_id])
     answer.keywords.create(keyword: params[:keyword], weight: params[:weight])
     flash[:success] = "Keyword added successfully!"
-    redirect_to action: "manage"
+    redirect_to controller: 'survey', action: 'edit_question', id: params[:question_id]
   end
 
+  def delete_question
+    question = Question.find(params[:id])
+    @questionsAfter = Question.where("position >= ?", question.position).order(position: :desc)
+    @questionsAfter.each do |q|
+      q.position -= 1
+    end
+    @questionsAfter.each do |q|
+      q.save!
+    end
+
+    question.destroy
+    if(question.destroyed?)
+      flash[:success] = "Question deleted successfully"
+    else
+      flash[:error] = "Question not deleted successfully: " + question.errors.full_messages.join(", ")
+    end
+    redirect_to action: "manage"
+  end
 
   def delete_keyword
     Keyword.find(params[:keyword_id]).destroy
     flash[:success] = "Keyword deleted successfully!"
-    redirect_to action: "manage"
+    redirect_to controller: 'survey', action: 'edit_question', id: params[:question_id]
   end
 
   def submit
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170802024044) do
+ActiveRecord::Schema.define(version: 20170812051517) do
 
   create_table "answers", force: :cascade do |t|
     t.string   "answer_title"
@@ -62,7 +62,6 @@ ActiveRecord::Schema.define(version: 20170802024044) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "position"
-    t.index ["position"], name: "index_questions_on_position", unique: true
   end
 
   create_table "responses", force: :cascade do |t|
