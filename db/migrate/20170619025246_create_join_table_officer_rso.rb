99b54f595fc35d8e1556020657f4778bf1aeb414
Tyler Walker <twalker@eecs.wsu.edu>
2017-09-01 14:56:26 -0700

    removed the RSO officer

--- db/migrate/20170619025246_create_join_table_officer_rso.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableOfficerRso < ActiveRecord::Migration[5.0]
-  def change
-    create_join_table :officers, :rsos do |t|
-      t.index [:officer_id, :rso_id]
-      
-    end
-  end
-end
