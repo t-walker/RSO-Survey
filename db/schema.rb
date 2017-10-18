d95f33f8a6feaec888bd21be37df0af2e84e526c
Tyler Walker <twalker@eecs.wsu.edu>
2017-10-18 10:41:41 -0700

    changing inet to string

--- db/schema.rb
+++ db/schema.rb
@@ -75,8 +75,8 @@ ActiveRecord::Schema.define(version: 20171018165412) do
     t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
